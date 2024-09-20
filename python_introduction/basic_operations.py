008963d6813b91d364184b8d6df3c6cd923bd08e
j1ere <jeremywambua106@gmail.com>
2024-09-20 12:13:06 +0300

    addition, subtraction and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum = number1+number2
+print(f'Sum of {number1} and {number2} is {sum}')
+
+subtraction = number1-number2
+print(f'Subtraction of {number1} and {number2} is {subtraction}')
+
+product = number1 * number2
+print(f'Multiplication of {number1} and {number2} is {product}')
