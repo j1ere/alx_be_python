5283982c76125181d2944cff8b50da07f82211fe
j1ere <jeremywambua106@gmail.com>
2024-09-20 13:37:53 +0300

    finance calculator formular fix

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -10,6 +10,7 @@ rate = 0.05
 time = 1
 interest =  principal * rate * time
 
-projected_savings = principal + interest
+#projected_savings = principal + interest
+projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
 
 print(f'Projected savings after one year, with interest, is: ${projected_savings}')
