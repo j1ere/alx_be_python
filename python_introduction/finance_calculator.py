02dd504052312b3c35ef4a9ec52fd8726a379c5d
j1ere <jeremywambua106@gmail.com>
2024-09-20 13:43:51 +0300

    monthly-savings code fix

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 monthly_income = input('Enter your monthly income:')
-total_monthly_expenses = input('Enter your total monthly expenses:')
+monthly_expenses = input('Enter your total monthly expenses:')
 
-print(monthly_income, total_monthly_expenses)
+print(monthly_income, monthly_expenses)
 
-monthly_savings = int(monthly_income) - int(total_monthly_expenses)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 
 principal = monthly_savings * 12
 rate = 0.05
