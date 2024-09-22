4962a2d6881eb1c91cda5fdf00588fd343ac315a
j1ere <jeremywambua106@gmail.com>
2024-09-22 16:05:35 +0300

    text formating correction

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,11 +2,11 @@ weather = input("What's the weather like today? (sunny/rainy/cold):")
 
 weather_input = weather.lower()
 
-if weather_input == 'sunny':
-    print('Wear a t-shirt and sunglasses.')
-elif weather_input == 'rainy':
+if weather_input == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather_input == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_input == 'cold':
-    print("Make sure you wear a warm coat and a scarf")
+elif weather_input == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
