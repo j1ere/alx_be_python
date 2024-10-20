2e695637edb4f9e79f0e79b2e13d139138b23842
j1ere <jeremywambua106@gmail.com>
2024-10-20 06:01:59 +0300

    programing paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self,account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if float(amount) > 0:
+            self.account_balance += amount
+        else:
+            return f"unable to deposit amount"
+
+    def withdraw(self, amount):
+        if 0 < self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
