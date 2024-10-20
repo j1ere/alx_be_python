11a1d0d4e57404eb086608eaa007954ca1bad6eb
j1ere <jeremywambua106@gmail.com>
2024-10-20 18:00:09 +0300

    simple calculator test

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,45 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class SimpleCalculatorTests(unittest.TestCase):
+    #arrange
+    def setUp(self):
+        """Set up SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """test the addition method"""
+        self.assertEqual(self.calc.add(5,2), 7)
+        self.assertEqual(self.calc.add(-1,1), 0)
+        self.assertEqual(self.calc.add(-5,-4), -9)
+    
+    def test_subtraction(self):
+        "test the subtraction method"
+        self.assertEqual(self.calc.subtract(5,2), 3)
+        self.assertEqual(self.calc.subtract(-1,1), -2)
+        self.assertEqual(self.calc.subtract(1,-1), 2)
+        self.assertEqual(self.calc.subtract(-1,-1), 0)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(5,2),10)
+        self.assertEqual(self.calc.multiply(-1,1), -1)
+        self.assertEqual(self.calc.multiply(1, -1), -1)
+        self.assertEqual(self.calc.multiply(-1,-1), 1)
+    
+    def test_zero_division(self):
+        """test that dividing by zero returns None"""
+        self.assertEqual(self.calc.divide(10,0), None)
+        self.assertEqual(self.calc.divide(-10, 0), None)
+
+    def test_normal_division(self):
+        """testing for normal division"""
+        self.assertEqual(self.calc.divide(4,2), 2)
+        self.assertEqual(self.calc.divide(-4,2), -2)
+        self.assertEqual(self.calc.divide(4,-2), -2)
+
+    
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
